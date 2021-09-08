955fac1445555cee7b745348287b7a15c961bc5d
albertoArmero <alberto@scandit.com>
2021-09-08 19:09:37 +0200

    1 file modified, 1 added

--- src/app/add-to-cart.service.ts
+++ src/app/add-to-cart.service.ts
@@ -1,61 +1,30 @@
-import { HttpClient } from "@angular/common/http";
-import { ActiveCartService, CartActions, BadRequestHandler, CartVoucherService } from "@spartacus/core";
+import { ActiveCartService} from "@spartacus/core";
 import { Barcode } from "scandit-sdk";
 import { Injectable } from '@angular/core';
+import { BARCODE_DICTIONARY } from './barcode-dictionary';
 
 @Injectable({providedIn:'root'})
 export class AddToCartService{
 
-constructor(private http: HttpClient, private activeCartService: ActiveCartService, private errorVoucher: CartVoucherService){
+constructor(private activeCartService: ActiveCartService){
  }
   
   addProductToCart(barcode: Barcode[]){
 
-    var mapped_product:string = this.mapBarcodeWithProduct(barcode[0].data);
-    this.activeCartService.addEntry(mapped_product,1);
-    //console.log('Product ' + mapped_product + ' has been added to the cart');
+    let mappedProduct:string = this.mapBarcodeWithProduct(barcode[0].data);
+    this.activeCartService.addEntry(mappedProduct, 1);
 
 }
 
-  mapBarcodeWithProduct(barcode_value: string): string{
-    var mapped_product:string;
+  mapBarcodeWithProduct(barcode: string): string{
 
-    switch(barcode_value) { 
-      case '5395215092097250130':{ 
-        mapped_product = '300938';
-        break; 
-      } 
-      case '5213848038594001059':{ 
-        mapped_product = '553637';
-        break; 
-      }
-      case '5561378049223501222':{ 
-        mapped_product = '1934793';
-        break; 
-      }
-      case '5337894712216009332':{ 
-        mapped_product = '1981415';
-        break; 
-      }
-      case '5181326608309400233':{ 
-        mapped_product = '816780';
-        break; 
-      }   
-      default: { 
-        //mapped_product = 'InvalidProductID';
-        //this.invalidProduct();
-        break; 
-      } 
-  } 
+    let productID:string;
 
-    return mapped_product;
+    productID = BARCODE_DICTIONARY[barcode];
+
+    return productID;
+
   }
 
-  invalidProduct(){
-
-    var cart_ID:string;
-    this.errorVoucher.addVoucher("invalidProductID",cart_ID);
-    
-  }
 
 }
